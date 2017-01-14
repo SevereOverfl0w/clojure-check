1deaeb67d87b6545a6e1199b8892a15c89daec61
Dominic Monroe <dominic@juxt.pro>
2017-01-14 17:30:31 +0000

    Initial Eastwood binary

---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"github.com/google/uuid"
+	bencode "github.com/jackpal/bencode-go"
+	"gopkg.in/edn.v1"
+	"fmt"
+	"net"
+	"os"
+)
+
+func stringInSlice(a string, list []string) bool {
+    for _, b := range list {
+        if b == a {
+            return true
+        }
+    }
+    return false
+}
+
+type Response struct {
+	Ex string
+	Value string
+	Status []string
+	Id string
+	Session string
+	Out string
+}
+
+type EastwoodArgs struct {
+    Namespaces []edn.Symbol `edn:",omitempty"`
+}
+
+func eastwood(args []string, conn net.Conn){
+    ns := make([]edn.Symbol, len(args))
+
+    for index, element := range args {
+	ns[index] = edn.Symbol(element)
+    }
+
+    x := EastwoodArgs{ns}
+    b, err := edn.Marshal(x)
+    code := fmt.Sprintf(`(do (require 'eastwood.lint) (eastwood.lint/eastwood '%v))`, string(b))
+
+    fmt.Println(code)
+
+    msguuid, _ := uuid.NewRandom()
+    msgid := msguuid.String()
+
+    instruction := map[string]interface{}{
+	"op": "eval",
+	"code": code,
+	"id": msgid,
+    }
+    err = bencode.Marshal(conn, instruction)
+
+    if err != nil {
+	fmt.Println(err)
+	return
+    }
+
+    for {
+	result := Response{}
+	err = bencode.Unmarshal(conn, &result)
+	if err != nil {
+	    fmt.Println(err)
+	    return
+	}
+	if result.Id != msgid {
+	    fmt.Println("Skipping this message id")
+	    continue
+	}
+	if result.Ex != "" {
+	    fmt.Println(result.Ex)
+	}
+
+	if result.Out != "" {
+	    fmt.Println("", result.Out)
+	}
+
+	// if result.Value != "" {
+	//     fmt.Println(result.Value)
+	// }
+
+	if len(result.Status) > 0 {
+	    return
+	    // if stringInSlice("done", result.Status) {
+		// return
+	    // }
+	}
+    }
+}
+
+func main() {
+    args := os.Args
+    if len(args) < 2 {
+	return
+    }
+    nrepl := args[1]
+
+    conn, err := net.Dial("tcp", nrepl)
+    if err != nil {
+	fmt.Println(err)
+	return
+    }
+    defer conn.Close()
+
+    eastwood(args[2:], conn)
+}
